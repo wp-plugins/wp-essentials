dd73a2ff64174d72093dbf44ebed93620b7e477d
iprogressltd <iprogressltd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-15 10:43:24 +0000

    Commit
    git-svn-id: https://plugins.svn.wordpress.org/wp-essentials/trunk@756641 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-essentials.php
@@ -0,0 +1,107 @@
+<?php
+	/*
+		Plugin Name: WP Essentials
+		Plugin URI: http://wordpress.iprogress.co.uk
+		Description: All-in-one bundle of essential plugins and functions for all WordPress websites.
+		Version: 1.0
+		Author: iprogress
+		Author URI: http://wordpress.iprogress.co.uk/plugins/wp-essentials/
+	*/
+	
+	// Essentails Setup
+		define('ESSENTIALS_VERSION', '1.0');
+		define('ESSENTIALS_DIR', dirname(__FILE__));
+		define('ESSENTIALS_PATH', plugins_url().'/wp-essentials');
+		
+	// Add Options		
+		function add_options() {
+			add_option('wpe_cleanup',1);
+			add_option('wpe_client_role',1);
+			add_option('wpe_custom_image_sizes',1);
+			add_option('wpe_error_reports',1);
+			add_option('wpe_error_reports_google_analytics',1);
+			add_option('wpe_error_reports_search_engines',1);
+			add_option('wpe_footer_link',0);
+			add_option('wpe_image_quality',75);
+			add_option('wpe_javascript',1);
+			add_option('wpe_google_analytics','');
+			add_option('wpe_php_date',1);
+			add_option('wpe_debug_mode',0);
+			add_option('wpe_facebook',1);
+			add_option('wpe_flickr_username','');
+			add_option('wpe_flickr_api','');
+			add_option('wpe_google_maps',1);
+			add_option('wpe_twitter_username','');
+			add_option('wpe_twitter_consumer_key','');
+			add_option('wpe_twitter_consumer_secret','');
+			add_option('wpe_twitter_oauth_access_token','');
+			add_option('wpe_twitter_oauth_access_token_secret','');
+			add_option('wpe_style','css');
+			add_option('wpe_email',1);
+			add_option('wpe_video',1);
+			add_option('wpe_excerpt',1);
+			add_option('wpe_get_image_source',1);
+			add_option('wpe_link_it',1);
+			add_option('wpe_relative_time',1);
+		}
+		
+		register_activation_hook(__FILE__,'add_options');
+		
+	// Load Plugins
+		function essentials_load(){			
+			// System
+				if (get_option('wpe_cleanup')==1) { include(ESSENTIALS_DIR."/system/cleanup.php"); }
+				if (get_option('wpe_client_role')==1) { include(ESSENTIALS_DIR."/system/client-role.php"); }
+				include(ESSENTIALS_DIR."/system/custom-image-sizes.php");
+				if (get_option('wpe_debug_mode')==1) { include(ESSENTIALS_DIR."/system/debug-mode.php"); }
+				if (get_option('wpe_error_reports')==1) { include(ESSENTIALS_DIR."/system/error-reports.php"); }
+				if (get_option('wpe_footer_link')==1) { include(ESSENTIALS_DIR."/system/footer-link.php"); }
+				include(ESSENTIALS_DIR."/system/image-quality.php");
+				if (get_option('wpe_javascript')==1) { include(ESSENTIALS_DIR."/system/javascript.php"); }
+			
+			// Plugins
+				if (get_option('wpe_email')==1) { include(ESSENTIALS_DIR."/plugins/email.php"); }
+				include(ESSENTIALS_DIR."/plugins/analytics.php");
+				if (get_option('wpe_facebook')==1) { include(ESSENTIALS_DIR."/plugins/facebook.php"); }
+				include(ESSENTIALS_DIR."/plugins/flickr.php");
+				if (get_option('wpe_google_maps')==1) { include(ESSENTIALS_DIR."/plugins/google-maps.php"); }
+				include(ESSENTIALS_DIR."/plugins/twitter.php");
+				
+			// Shortcodes
+				if (get_option('wpe_php_date')==1) { include(ESSENTIALS_DIR."/shortcodes/php-date.php"); }
+				if (get_option('wpe_video')==1) { include(ESSENTIALS_DIR."/shortcodes/video.php"); }
+				
+			// Custom Functions
+				if (get_option('wpe_excerpt')==1) { include(ESSENTIALS_DIR."/functions/excerpt.php"); }
+				if (get_option('wpe_get_image_source')==1) { include(ESSENTIALS_DIR."/functions/get-image-source.php"); }
+				if (get_option('wpe_link_it')==1) { include(ESSENTIALS_DIR."/functions/link-it.php"); }
+				if (get_option('wpe_relative_time')==1) { include(ESSENTIALS_DIR."/functions/relative-time.php"); }
+		}
+		add_action('widgets_init', 'essentials_load');
+		
+	// Load Scripts & Styles
+		function wpe_scripts() {
+			$screen = get_current_screen();
+			if ($screen->base == 'toplevel_page_wp-essentials') {
+				wp_enqueue_script('wpe_slider',ESSENTIALS_PATH.'/scripts/wpe-slider.js',false);
+				wp_enqueue_script('wpe_settings',ESSENTIALS_PATH.'/scripts/wpe-settings.js',false);
+				wp_enqueue_style('wpe_styles',ESSENTIALS_PATH.'/styles/wpe-style.css',false);
+			}
+		}
+		add_action('admin_enqueue_scripts','wpe_scripts');
+		
+	// Load jQuery
+		if (!is_admin()) wp_enqueue_script("jquery");
+	
+	// Add Essentails Menu
+		function wpe_menu() {
+			add_menu_page('WP Essentials', 'WP Essentials', 'manage_options', 'wp-essentials', 'wpe_function',plugins_url('wp-essentials/images/icon.png'));
+		}
+		
+		function wpe_function() {
+			if (!current_user_can('manage_options'))  {
+				wp_die( __('You do not have sufficient permissions to access this page.') );
+			}
+			include("settings.php");
+		}
+		add_action('admin_menu', 'wpe_menu');
