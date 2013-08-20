5b07a36c8c8b6ef2093449e4a531cb7214fda296
iprogressltd <iprogressltd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-20 14:34:12 +0000

    Added nofollow to footer link
    git-svn-id: https://plugins.svn.wordpress.org/wp-essentials/trunk@759307 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- system/footer-link.php
+++ system/footer-link.php
@@ -1,7 +1,7 @@
 <?php
 	if (!function_exists('wpe_footer_link')) {
 		function wpe_footer_link() {
-			echo '<p style="font-size:10px;text-align:center;margin:13px 0px;">Powered by <a href="http://www.wp-essentials.net">WP Essentials</a></p>';	
+			echo '<p style="font-size:10px;text-align:center;margin:13px 0px;">Powered by <a href="http://www.wp-essentials.net" target="_blank">WP Essentials</a></p>';	
 		}
 		add_action('wp_footer', 'wpe_footer_link');
 	}
