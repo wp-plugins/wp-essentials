59befff2af849d4e7edd7421e869ee482924399d
iprogressltd <iprogressltd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-21 09:42:39 +0000

    Updated Twitter search link
    git-svn-id: https://plugins.svn.wordpress.org/wp-essentials/trunk@759826 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugins/twitter.php
+++ plugins/twitter.php
@@ -349,7 +349,7 @@
 				$tweet = preg_replace('/(^|\s)@(\w+)/','\1<a href="http://www.twitter.com/\2" target="_blank" rel="nofollow">@\2</a>',$tweet);
 				$tweet = preg_replace("#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t<]*)#ise", "'\\1<a href=\"\\2\" rel=\"nofollow\" target=\"_blank\">\\2</a>'", $tweet);
 				$tweet = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r<]*)#ise", "'\\1<a href=\"http://\\2\" rel=\"nofollow\" target=\"_blank\">\\2</a>'", $tweet);
-				$tweet = preg_replace('/(^|\s)#(\w+)/','\1<a href="http://search.twitter.com/search?q=%23\2" rel="nofollow" target="_blank">#\2</a>',$tweet);
+				$tweet = preg_replace('/(^|\s)#(\w+)/','\1<a href="https://twitter.com/search?q=%23\2&mode=realtime" rel="nofollow" target="_blank">#\2</a>',$tweet);
 				return $tweet;
 			}
 		}
