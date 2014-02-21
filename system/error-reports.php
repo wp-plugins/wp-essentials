aa2cd42c0bc4f69fa55bc13e0ca8664255ec2a7c
iprogressltd <iprogressltd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-21 15:12:00 +0000

    Update to 1.0.7
    git-svn-id: https://plugins.svn.wordpress.org/wp-essentials/tags/1.0.7@862394 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ system/error-reports.php
@@ -0,0 +1,34 @@
+<?php
+	if (!function_exists('error_report')) {
+		function error_report() {
+			$errors = '';
+			
+			if (!get_option('wpe_google_analytics')&&get_option('wpe_error_reports_google_analytics')) {
+				$errors .= '<li>Your website does not have Google Analytics tracking installed. <a href="'.get_bloginfo('wpurl').'/wp-admin/admin.php?page=wp-essentials">Fix &raquo;</a></li>';
+			}
+			
+			if (!get_option('blog_public')&&get_option('wpe_error_reports_search_engines')) {
+				$errors .= '<li>Search engines are blocked from this website. <a href="'.get_bloginfo('wpurl').'/wp-admin/options-reading.php">Fix &raquo;</a></li>';
+			}
+			
+			if (get_option('wpe_instagram_username') && get_option('wpe_instagram_client_id') && !get_option('wpe_instagram_user_id') && !get_option('wpe_instagram_access_token')) {
+				$errors .= '<li>Your Instagram set up is almost complete. <a href="'.get_bloginfo('wpurl').'/wp-admin/admin.php?page=wp-essentials">Fix &raquo;</a></li>';
+			}
+			
+			if (username_exists('admin')&&get_option('wpe_error_reports_check_username')) { 
+				$errors .= '<li>The username <code>admin</code> is vulnerable to attacks. Please set up a new user and delete <code>admin</code> <a href="'.get_bloginfo('wpurl').'/wp-admin/user-new.php">Fix &raquo;</a></li>';
+			}
+			
+			if ($errors) {
+				echo '
+					<div class="error">
+						<p>The following issues have been found:</p>
+						<ul style="margin:0px 20px;list-style:disc;">
+							'.$errors.'
+						</ul>
+					</div>
+				';
+			}
+		}
+		add_filter('admin_notices', 'error_report');
+	}
